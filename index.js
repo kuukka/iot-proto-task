150a473d5ab5fd7d3bcdb10ca9832bc5e6d8cb10
Petri Kataja <peejkoo@gmail.com>
2022-03-06 19:33:06 +0200

    Another try on rest route

--- index.js
+++ index.js
@@ -40,7 +40,9 @@ app.use('/api/books', bookRoutes);
 
 
 // Rest goes to frontend
-app.use('/', express.static(__dirname+"/build"));
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/build/index.html'));
+});
 
 // app.get('/', (req, res) => {
 //     res.json({
