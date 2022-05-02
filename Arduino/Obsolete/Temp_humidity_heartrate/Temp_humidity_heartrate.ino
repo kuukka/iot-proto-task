1e17e8d5d4ed35822a9455dcd24841fba76e00ef
Tuomas Kuukka <103113741+kuukka@users.noreply.github.com>
2022-05-02 16:22:06 +0300

    Clarify folder structure

--- Arduino/Obsolete/Temp_humidity_heartrate/Temp_humidity_heartrate.ino
+++ Arduino/Obsolete/Temp_humidity_heartrate/Temp_humidity_heartrate.ino
@@ -2,18 +2,25 @@
 
 #include <Wire.h> //include Wire.h library
 
+TH02_dev TH02;
+
 void setup() {
+  TH02.begin();
   Wire.begin(); // Wire communication begin
   Serial.begin(9600); // The baudrate of Serial monitor is set in 9600
   //while (!Serial); // Waiting for Serial Monitor
   //Wire.onReceive(receiveEvent);
 
+  
   Serial.println("start");
 }
 
 void loop() {
 
+  Serial.print("test: ");
+
   float temper = TH02.ReadTemperature();
+  Serial.print("test2 ");
   float humidity = TH02.ReadHumidity();
 
   Serial.print("Humidity: ");
